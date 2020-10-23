1759624e0f5321dea4b87f903d4941fd5a0fecd8
Bertie Blackman <blackmanrgh@gmail.com>
2020-10-23 16:57:46 +0100

    Git hooks

--- scripts/config-check.sh
+++ scripts/config-check.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-# cp ./ci/mock_secrets.yaml ./secrets.yaml
-cp -r ./ci/ssl/ ./ssl/
-docker run --name=home-assistant -v "$PWD"/:/config homeassistant/home-assistant:stable bash -c "python -m homeassistant --script check_config --config ./ --info all"
-
 docker kill home-assistant || :
 docker rm home-assistant || :
+
+cp ./ci/mock_secrets.yaml ./secrets.yaml
+cp -r ./ci/ssl/ ./ssl/
+configCheckOutput=$(docker run --name=home-assistant -v "$PWD"/:/config homeassistant/home-assistant:stable bash -c "python -m homeassistant --script check_config --config ./ --info all")
+
+if [[ $configCheckOutput == *"Failed config"* ]]; then
+  exit 1
+fi
