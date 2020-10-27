ae19916d61301aaf2bdd552f58376c150159d461
Bertie Blackman <blackmanrgh@gmail.com>
2020-10-27 11:26:50 +0000

    Full run

--- scripts/sync-config.sh
+++ scripts/sync-config.sh
@@ -3,4 +3,9 @@
 user="$1"
 host="$2"
 
-rsync -avO --no-perms ./config/ "$user@$host:/config/"
+rsync -avO \
+  --no-perms \
+  --include={'automations/***','customizations/***','entities/***','frontend/***','integrations/***','scenes/***','scripts/***','themes/***','automations.yaml','configuration.yaml','ui-lovelace.yaml'} \
+  --exclude="*" \
+  --delete \
+  ./config/ "$user@$host:/config/"
