b77982b4841d7615b8937f9e38163c512d846225
Bertie Blackman <blackmanrgh@gmail.com>
2020-10-27 11:50:56 +0000

    Refactors config (#153)

--- scripts/config-check.sh
+++ scripts/config-check.sh
@@ -6,12 +6,12 @@ function killDocker() {
 }
 
 function copyStubbedData() {
-  cp ./ci/mock_secrets.yaml ./secrets.yaml
-  cp -r ./ci/ssl/ ./ssl/
+  cp ./.stubs/mock_secrets.yaml ./config/secrets.yaml
+  cp -r ./.stubs/ssl/ ./config/ssl/
 }
 
 function checkHaConfig() {
-  configCheckOutput=$(docker run --name=home-assistant -v "$PWD"/:/config homeassistant/home-assistant:stable bash -c "python -m homeassistant --script check_config --config ./ --info all")
+  configCheckOutput=$(docker run --name=home-assistant -v "$PWD"/config:/config homeassistant/home-assistant:stable bash -c "python -m homeassistant --script check_config --config ./ --info all")
 
   if [[ $configCheckOutput == *"Failed config"* ]]; then
     echo "$configCheckOutput"
@@ -22,6 +22,7 @@ function checkHaConfig() {
 }
 
 function cleanWorkingDirectory() {
+  cd ./config || exit 1
   rm -rf .cloud .storage .HA_VERSION home-assistant_v2.db home-assistant.log secrets.yaml ssl
 }
 
---
+++ scripts/sync-config.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+user="$1"
+host="$2"
+restartWebhookURL="$3"
+
+rsync -avO \
+  --no-perms \
+  --include={'automations/***','customizations/***','entities/***','frontend/***','integrations/***','scenes/***','scripts/***','themes/***','automations.yaml','configuration.yaml','ui-lovelace.yaml'} \
+  --exclude="*" \
+  --delete \
+  ./config/ "$user@$host:/config/"
+
+curl -X POST "$restartWebhookURL"
