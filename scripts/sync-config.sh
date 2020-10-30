ff097d2c8da8e89ccb2f35d4865c1a4a9d31eb92
Bertie Blackman <blackmanrgh@gmail.com>
2020-10-30 15:39:32 +0000

    Refactors www structure and puts under VC

--- scripts/sync-config.sh
+++ scripts/sync-config.sh
@@ -6,7 +6,7 @@ restartWebhookURL="$3"
 
 rsync -avO \
   --no-perms \
-  --include={'automations/***','customizations/***','entities/***','lovelace/***','integrations/***','scenes/***','scripts/***','themes/***','automations.yaml','configuration.yaml','ui-lovelace.yaml'} \
+  --include={'automations/***','customizations/***','entities/***','lovelace/***','integrations/***','scenes/***','scripts/***','themes/***','www/***','automations.yaml','configuration.yaml','ui-lovelace.yaml'} \
   --exclude="*" \
   --delete \
   ./config/ "$user@$host:/config/"
