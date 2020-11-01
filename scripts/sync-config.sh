28bc3ec31568b5d14bb925eb9a51c1a72d7939c9
Bertie Blackman <blackmanrgh@gmail.com>
2020-11-01 21:01:02 +0000

    Sync config script

--- scripts/sync-config.sh
+++ scripts/sync-config.sh
@@ -4,11 +4,31 @@ user="$1"
 host="$2"
 restartWebhookURL="$3"
 
-rsync -avO \
-  --no-perms \
-  --include={'automations/***','customizations/***','entities/***','lovelace/***','integrations/***','scenes/***','scripts/***','themes/***','www/***','custom_components/***','automations.yaml','configuration.yaml','ui-lovelace.yaml'} \
-  --exclude="*" \
-  --delete \
-  ./config/ "$user@$host:/config/"
+if [[ -z "$user" || -z "$host" ]]; then
+  echo "Missing either user or host inputs"
+  exit 1
+fi
 
-curl -X POST "$restartWebhookURL"
+function syncFiles() {
+  echo "Synching files..."
+
+  rsync -avO \
+    --no-perms \
+    --include={'automations/***','customizations/***','entities/***','lovelace/***','integrations/***','scenes/***','scripts/***','themes/***','www/***','custom_components/***','automations.yaml','configuration.yaml','ui-lovelace.yaml'} \
+    --exclude="*" \
+    --delete \
+    ./config/ "$user@$host:/config/"
+
+  echo "Done!"
+}
+
+function restartServer() {
+  echo "Restarting server..."
+  curl -X POST "$restartWebhookURL"
+}
+
+syncFiles
+
+if [[ -n "$restartWebhookURL" ]]; then
+  restartServer
+fi
