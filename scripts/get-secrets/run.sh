e001bc593551e5f7ed76dad08902ef1c0baf4612
Bertie Blackman <blackmanrgh@gmail.com>
2020-11-12 13:09:56 +0000

    Get secrets from vault

--- scripts/get-secrets/run.sh
+++ scripts/get-secrets/run.sh
@@ -6,19 +6,14 @@ password=$3
 secretKey=$4
 deviceID=$5
 
-function checkVersion() {
-    echo "1Password version: $(op --version)"
-}
-
 function signIn() {
-    export OP_DEVICE="$deviceID"
-    eval "$(echo "$password" | op signin "$signInAddress" "$emailAddress" "$secretKey")"
+  export OP_DEVICE="$deviceID"
+  eval "$(echo "$password" | op signin "$signInAddress" "$emailAddress" "$secretKey")"
 }
 
 function getHASecrets() {
-    op list items --tags ha-secrets | op get item - --fields notesPlain
+  op list items --tags ha-secrets | op get item - --fields notesPlain
 }
 
-checkVersion
 signIn
 getHASecrets
